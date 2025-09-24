a440865de76c188c29ab44753b596d44126fd8c2
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2025-09-24 10:58:58 +0100

    Allow redirect to content

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,15 +13,9 @@ class QuestionsController < ApplicationController
     session[:answers][questionnaire.id.to_s] ||= {}
     session[:answers][questionnaire.id.to_s][question.id.to_s] = selected_answer
     session[:score] ||= 0
-    session[:score] += answer[:score] if answer[:score].present?
+    session[:score] += answer[:score] if answer && answer[:score].present?
 
-    next_question = answer[:next]
-    path = if next_question
-             next_question_path
-           else
-             questionnaire_questions_path(questionnaire_id: questionnaire.id)
-           end
-    redirect_to path
+    redirect_to next_question_path || result_page_path
   end
 
 private
@@ -45,15 +39,23 @@ private
   # if next is in form "one/two" next is to questionnaire one, question two
   # if next is in form "three" next is to question three of current questionnaire
   def next_question_path
-    next_path = answer[:next]
+    next_path = answer && answer[:next]
     return if next_path.blank?
 
     elements = next_path.split("/")
     elements.unshift(questionnaire.id) if elements.size == 1
 
-    questionnaire_question_path(
-      questionnaire_id: elements[0],
-      id: elements[1],
-    )
+    if elements[0] == "content"
+      content_path(elements[1])
+    else
+      questionnaire_question_path(
+        questionnaire_id: elements[0],
+        id: elements[1],
+      )
+    end
+  end
+
+  def result_page_path
+    questionnaire_questions_path(questionnaire_id: questionnaire.id)
   end
 end
